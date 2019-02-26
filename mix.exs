23f831164ccd30c02e204c7e1dcc3efb687d33e9
Artur Plysyuk <ifuelen@gmail.com>
2019-02-26 19:13:18 +0200

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bamboo.FallbackAdapter.MixProject do
   def project do
     [
       app: :bamboo_fallback,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
